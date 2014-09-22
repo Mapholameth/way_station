6817c926278c0c2670ac2f471619bde239065fa0
Denis Rotanov <mapholameth@gmail.com>
2014-09-22 15:48:01 +1100

    First working version of mcmf algorithm. Using DijkstraSearch without potentials i.e. solving for not all cases.

--- adv-alg-2014/min-cost-max-flow/main.cpp
+++ adv-alg-2014/min-cost-max-flow/main.cpp
@@ -2,52 +2,108 @@
 #include <cstdlib>
 #include <cstring>
 #include <limits>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-const int maxN = 1000;
-const int infWeight = std::numeric_limits<int>::max();
-bool visited[maxN];
-int distance[maxN];
+struct Edge
+{
+  int from;
+  int to;
+  int bandwidth;
+  int cost;
+  int saturation;
+  int roadIndex; // refers to road index from input file
+  Edge* inverse;
 
-int n;
-int m;
-int s;
-int edges[maxN][maxN];
+  Edge(int from, int to, int bandwidth, int cost, int roadIndex)
+    : from(from)
+    , to(to)
+    , bandwidth(bandwidth)
+    , cost(cost)
+    , roadIndex(roadIndex)
+    , saturation(0)
+    , inverse(NULL)
+  {
+
+  }
+};
+
+const double infTime = std::numeric_limits<double>::max();
+
+int n; // 2 <= n <= 200, vertice count
+int m; // 1 <= m <= 2000, edge count
+int k; // 1 <= k <= 100, flow count
+// s = 0 - source index, t = n - 1 - sink index
+const int maxN = 200;
+vector<Edge> edges[maxN];
+vector<vector<int> > roads;
+pair<int, int> path[maxN];
+int totalCost = 0;
+
+bool DijkstraSearch();
+void Backtrace();
 
 int main()
 {
-  for (int i = 0; i < maxN; i++)
-  {
-    for (int j = 0; j < maxN; j++)
-    {
-      edges[i][j] = infWeight;
-    }
-  }
-
-  FILE* fi = fopen("input.txt", "r");
-  fscanf(fi, "%d%d%d", &n, &m, &s);
-  s--;
+  FILE* fi = fopen("brides.in", "r");
+  fscanf(fi, "%d%d%d", &n, &m, &k);
   for (int i = 0; i < m; i++)
   {
-    int u;
-    int v;
-    int w;
-    fscanf(fi, "%d%d%d", &u, &v, &w);
-    edges[u - 1][v - 1] = w;
+    int v0;
+    int v1;
+    int time;
+    fscanf(fi, "%d%d%d", &v0, &v1, &time);
+    v0--;
+    v1--;
+    edges[v0].push_back(Edge(v0, v1, 1, time, i + 1));
+    edges[v1].push_back(Edge(v1, v0, 1, time, i + 1));
   }
   fclose(fi);
 
-  for (int i = 0; i < n; i++)
+  FILE* fo = fopen("brides.out", "w");
+
+  for (int i = 0; i < k; i++)
   {
-    distance[i] = infWeight;
+    if (!DijkstraSearch())
+    {
+      fprintf(fo, "-1");
+      fclose(fo);
+      return 0;
+    }
+
+    Backtrace();
   }
 
-  distance[s] = 0;
+  fprintf(fo, "%.5f\n", static_cast<double>(totalCost) / k);
+  for (int i = 0; i < roads.size(); i++)
+  {
+    fprintf(fo, "%d ", roads[i].size());
+    for (int j = 0; j < roads[i].size(); j++)
+    {
+      fprintf(fo, "%d ", roads[i][j]);
+    }
+    fprintf(fo, "\n");
+  }
+
+  fclose(fo);
+  return 0;
+}
+
+bool DijkstraSearch()
+{
+  const int infWeight = std::numeric_limits<int>::max();
+  static bool visited[maxN];
+  static int distance[maxN];
+  fill(visited, visited + maxN, false);
+  fill(distance, distance + maxN, infWeight);
+  fill(path, path + maxN, make_pair(-1, -1));
+  distance[0] = 0;
 
   for (int i = 0; i < n; i++)
   {
-    int v = -1;
+    int u = -1;
     int minWeight = infWeight;
     for (int j = 0; j < n; j++)
     {
@@ -55,42 +111,59 @@ int main()
           && distance[j] < minWeight)
       {
         minWeight = distance[j];
-        v = j;
+        u = j;
       }
     }
 
-    if (v == -1)
+    if (u == -1)
     {
       break;
     }
 
-    visited[v] = true;
-    for (int u = 0; u < n; u++)
+    visited[u] = true;
+    for (int j = 0; j < edges[u].size(); j++)
     {
-      if (!visited[u]
-          && edges[v][u] != infWeight)
+      int v = edges[u][j].to;
+      if (!visited[v]
+          && edges[u][j].saturation < edges[u][j].bandwidth)
       {
-        if (distance[u] > distance[v] + edges[v][u])
+        if (distance[v] > distance[u] + edges[u][j].cost)
         {
-          distance[u] = distance[v] + edges[v][u];
+          distance[v] = distance[u] + edges[u][j].cost;
+          path[v].first = u;
+          path[v].second = j;
         }
       }
     }
   }
 
-  FILE* fo = fopen("output.txt", "w");
-  for (int i = 0; i < n; i++)
-  {
-    if (distance[i] == infWeight)
-    {
-      fprintf(fo, "-1 ");
-    }
-    else
-    {
-      fprintf(fo, "%d ", distance[i]);
-    }
-  }
-  fclose(fo);
+  return distance[n - 1] != infWeight;
+}
 
-  return 0;
+void Backtrace()
+{
+  roads.push_back(vector<int>());
+  vector<int>& road = roads.back();
+
+  int i = n - 1;
+  while (path[i].first != -1)
+  {
+    Edge& e = edges[path[i].first][path[i].second];
+
+    if (!e.inverse)
+    {
+      Edge e_i = Edge(e.to, e.from, 0, -e.cost, e.roadIndex);
+      edges[e.to].push_back(e_i);
+      e.inverse = &edges[e.to].back();
+    }
+    e.saturation += 1;
+    e.inverse->saturation -= 1;
+
+    totalCost += e.cost;
+    road.push_back(e.roadIndex);
+
+    i = path[i].first;
+  }
+
+  std::reverse(road.begin(), road.end());
 }
