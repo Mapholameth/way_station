186c14b8d285d888f9139dec31948d19eecc4115
Denis Rotanov <mapholameth@gmail.com>
2014-09-26 08:59:12 +1100

    Fix suffix tree construction algorithm.

--- adv-alg-2014/suffix-tree-ukkonen/main.cpp
+++ adv-alg-2014/suffix-tree-ukkonen/main.cpp
@@ -1,248 +1,182 @@
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
-#include <algorithm>
+#include <string>
+#include <map>
+#include <limits>
+
+const int oo = std::numeric_limits<int>::max();
+const int MAXN = 250000;
 
 using namespace std;
 
-unsigned char s[250001];
-int c = 0;
-
-struct Node;
-
-struct Edge
-{
-  Node* node;
-  int i0;
-  int i1;
-
-  int i1Real()
-  {
-    return i1 == -1 ? c : i1;
-  }
-
-  Edge(int i0, int i1)
-    : i0(i0)
-    , i1(i1)
-    , node(NULL)
-  {
-
-  }
-};
+int root = 0;
+int lastAdded = 0;
+int pos = 0;
+int needSL = 0;
+int remainder = 0;
+int activeNode = 0;
+int activeEdge = 0;
+int activeLength = 0;
+char text[MAXN];
 
 struct Node
 {
-  Edge* edges[256];
-  Node* suffixLink;
+  // [start; end)
+  int start;
+  int end;
+  int slink;
+  map<unsigned char, int> next;
 
-  Node()
-    : suffixLink(NULL)
+  int Length()
   {
-    fill(edges, edges + 256, static_cast<Edge*>(NULL));
+    return min(end, pos + 1) - start;
   }
 };
 
-Node root;
-Node* activeNode = &root;
-unsigned char activeEdge = 0;
-int activeLength = 0;
-int debt = 0;
+Node tree[MAXN * 2];
 
-void Print(Node* node, int l = 0)
+int MakeNewNode(int start, int end = oo)
 {
-  if (node == NULL)
+  Node nd;
+  nd.start = start;
+  nd.end = end;
+  nd.slink = 0;
+  tree[++lastAdded] = nd;
+  return lastAdded;
+}
+
+char GetActiveEdge()
+{
+  return text[activeEdge];
+}
+
+void AddSuffixLink(int node)
+{
+  if (needSL > 0)
   {
-    return;
+    tree[needSL].slink = node;
   }
+  needSL = node;
+}
+
+bool WalkDown(int node)
+{
+  if (activeLength >= tree[node].Length())
+  {
+    activeEdge += tree[node].Length();
+    activeLength -= tree[node].Length();
+    activeNode = node;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void PushLetter()
+{
+  needSL = 0;
+  remainder++;
+
+  while(remainder > 0)
+  {
+    if (activeLength == 0)
+    {
+      activeEdge = pos;
+    }
+
+    if (tree[activeNode].next.count(GetActiveEdge()) == 0)
+    {
+      int leaf = MakeNewNode(pos);
+      tree[activeNode].next[GetActiveEdge()] = leaf;
+      AddSuffixLink(activeNode);
+    }
+    else
+    {
+      int next = tree[activeNode].next[GetActiveEdge()];
+      if (WalkDown(next))
+      {
+        continue;
+      }
+
+      if (text[tree[next].start + activeLength] == text[pos])
+      {
+        activeLength++;
+        AddSuffixLink(activeNode);
+        break;
+      }
+
+      int split = MakeNewNode(tree[next].start, tree[next].start + activeLength);
+      tree[activeNode].next[GetActiveEdge()] = split;
+      int leaf = MakeNewNode(pos);
+      tree[split].next[text[pos]] = leaf;
+      tree[next].start += activeLength;
+      tree[split].next[text[tree[next].start]] = next;
+      AddSuffixLink(split);
+    }
+    remainder--;
+
+    if (activeNode == root
+        && activeLength > 0)
+    {
+      activeLength--;
+      activeEdge = pos - remainder + 1;
+    }
+    else
+    {
+      activeNode = tree[activeNode].slink > 0 ? tree[activeNode].slink : root;
+    }
+  }
+}
+
+void Print(int node_id, int l = 0)
+{
   for (int i = 0; i < 256; i++)
   {
-    if (node->edges[i] != NULL)
+    if (tree[node_id].next[i] != 0)
     {
-      Edge& e = *node->edges[i];
-      int i1 = e.i1;
-      if (i1 == -1)
-      {
-        i1 = c;
-      }
+      Node e = tree[tree[node_id].next[i]];
       for (int j = 0; j < l; j++)
       {
         printf("  ");
       }
-      printf("%d %d : %c : ", e.i0, e.i1, i);
-      for (int j = e.i0; j <= i1; j++)
+      int end = e.end;
+      end = end == oo ? pos + 1 : end;
+      printf("%d %d : %c : ", e.start, end, i);
+      for (int j = e.start; j < end; j++)
       {
-        printf("%c", s[j]);
+        printf("%c", text[j]);
       }
       printf("\n");
-      Print(e.node, l + 1);
+      Print(tree[node_id].next[i], l + 1);
     }
   }
   if (l == 0)
   {
-    printf("at c = %d\n", c);
+    printf("at c = %d\n", pos);
     printf("================================================================================\n");
-  }
-}
-
-void PushLetter(unsigned char newLetter)
-{
-  unsigned char presentLetter = 0;
-  if (activeEdge != 0)
-  {
-    int i0 = activeNode->edges[activeEdge]->i0;
-    int i1 = activeNode->edges[activeEdge]->i1Real();
-    if (i1 < i0 + activeLength + 1)
-    {
-      if (activeNode->edges[activeEdge]->node->edges[newLetter])
-      {
-        activeNode = activeNode->edges[activeEdge]->node;
-        activeEdge = newLetter;
-        activeLength = -1;
-        presentLetter = newLetter;
-      }
-    }
-    else
-    {
-      presentLetter = s[activeNode->edges[activeEdge]->i0 + activeLength + 1];
-    }
-
-  }
-  else if (activeNode->edges[newLetter] != NULL)
-  {
-    presentLetter = newLetter;
-  }
-
-  Node* lastMadeNode = NULL;
-
-  if (presentLetter != newLetter)
-  {
-    while (debt > 0)
-    {
-      Edge& e = *activeNode->edges[activeEdge];
-      int i1 = e.i1;
-      e.i1 = e.i0 + activeLength;
-      Node* tempNode = e.node;
-      if (e.i1 != i1)
-      {
-        e.node = new Node();
-        e.node->edges[presentLetter] = new Edge(e.i0 + activeLength + 1, i1);
-        e.node->edges[presentLetter]->node = tempNode;
-      }
-      e.node->edges[newLetter] = new Edge(c, -1);
-      debt--;
-
-      if (lastMadeNode != NULL)
-      {
-        lastMadeNode->suffixLink = e.node;
-      }
-      lastMadeNode = e.node;
-
-      if (activeNode == &root)
-      {
-        activeEdge = s[c - debt];
-        activeLength--;
-      }
-      else
-      {
-        if (activeNode->suffixLink == 0)
-        {
-          activeNode = &root;
-        }
-        else
-        {
-          activeNode = activeNode->suffixLink;
-        }
-        {
-          int i0 = activeNode->edges[activeEdge]->i0;
-          int i1 = activeNode->edges[activeEdge]->i1;
-          if (i1 == -1)
-          {
-            i1 = c;
-          }
-          while (activeNode->edges[activeEdge]->i0 + activeLength > i1)
-          {
-            if (activeNode->edges[activeEdge]->node != NULL)
-            {
-              activeNode = activeNode->edges[activeEdge]->node;
-              activeEdge = s[c - debt + i1 - i0 + 1];
-              activeLength -= (i1 - i0 + 1);
-            }
-            if (!activeNode->edges[activeEdge])
-            {
-              activeNode->edges[activeEdge] = new Edge(c - debt + i1 - i0 + 1, -1);
-            }
-            i0 = activeNode->edges[activeEdge]->i0;
-            i1 = activeNode->edges[activeEdge]->i1;
-            if (i1 == -1)
-            {
-              i1 = c;
-            }
-          }
-        }
-      }
-    }
-    if (activeNode->edges[newLetter] == NULL)
-    {
-      activeEdge = 0;
-      activeLength = 0;
-      activeNode->edges[newLetter] = new Edge(c, -1);
-    }
-  }
-
-  if (presentLetter == newLetter)
-  {
-    if (activeEdge == 0)
-    {
-      activeEdge = newLetter;
-    }
-    else
-    {
-      activeLength++;
-    }
-    debt++;
-
-    int i0 = activeNode->edges[activeEdge]->i0;
-    int i1 = activeNode->edges[activeEdge]->i1;
-    if (i1 == -1)
-    {
-      i1 = c;
-    }
-    while (activeNode->edges[activeEdge]->i0 + activeLength > i1)
-    {
-      if (activeNode->edges[activeEdge]->node != NULL)
-      {
-        activeNode = activeNode->edges[activeEdge]->node;
-        activeEdge = s[i1 + 1];
-        activeLength -= (i1 - i0 + 1);
-      }
-      i0 = activeNode->edges[activeEdge]->i0;
-      i1 = activeNode->edges[activeEdge]->i1;
-      if (i1 == -1)
-      {
-        i1 = c;
-      }
-    }
   }
 }
 
 bool CheckWord(unsigned char* word)
 {
-  Node* node = &root;
+  Node n = tree[root];
   int i = 0;
   unsigned char letter = word[i];
   while (letter != 0)
   {
-    if (node->edges[letter] != NULL)
+    if (n.next.count(letter) != 0)
     {
-      Edge& e = *node->edges[letter];
-      for (int j = e.i0; j <= e.i1Real(); j++)
+      Node e = tree[n.next[letter]];
+      int i0 = e.start;
+      int i1 = e.end == oo ? pos + 1 : e.end;
+      for (int j = i0; j < i1; j++)
       {
         if (word[i] == 0)
         {
           return 1;
         }
-        if (s[j] != word[i])
+        if (text[j] != word[i])
         {
           return 0;
         }
@@ -253,9 +187,9 @@ bool CheckWord(unsigned char* word)
     {
       return 0;
     }
-    node = node->edges[letter]->node;
+    n = tree[n.next[letter]];
     letter = word[i];
-    if (node == NULL
+    if (n.next[letter] == 0
         && letter != 0)
     {
       return 0;
@@ -266,19 +200,21 @@ bool CheckWord(unsigned char* word)
 
 int main()
 {
-  FILE* fi = fopen("_input.txt", "r");
-  fscanf(fi, "%s", s);
+  FILE* fi = fopen("input.txt", "r");
+  fscanf(fi, "%s", text);
 
-  while (s[c] != 0)
+  root = activeNode = MakeNewNode(-1, -1);
+
+  while (text[pos] != 0)
   {
-    PushLetter(s[c]);
-//    Print(&root);
-    c++;
+    PushLetter();
+//    Print(root);
+    pos++;
   }
 
   int n;
   fscanf(fi, "%d", &n);
-  unsigned char word[1001];
+  unsigned char word[1000];
 
   FILE* fo = fopen("output.txt", "w");
 
