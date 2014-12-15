091b7c4b6aed1fb24dcc2e1808919fb77b3303b2
Denis Rotanov <mapholameth@gmail.com>
2014-12-15 11:47:54 +1000

    Begin work on micex nn prediction

---
+++ male-dami-2014/micex-vanga.py
@@ -0,0 +1,81 @@
+import random
+import csv
+
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure.modules import TanhLayer, SoftmaxLayer, GaussianLayer,\
+    SigmoidLayer, LinearLayer, GateLayer
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.supervised.trainers import RPropMinusTrainer
+
+from pykalman import KalmanFilter
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# <DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>
+
+# GAZP_131101_141031.txt
+# GMKN_131101_141031.txt
+# LKOH_131101_141031.txt
+# MGNT_131101_141031.txt
+# ROSN_131101_141031.txt
+# SBER_131101_141031.txt
+# SNGS_131101_141031.txt
+# URKA_131101_141031.txt
+# VTBR_131101_141031.txt
+
+csv_file = open('data/GAZP_131101_141031.txt')
+reader = csv.reader(csv_file, dialect="excel")
+
+x = []
+openPrice = []
+maxPrice = []
+minPrice = []
+closePrice = []
+volumeSold = []
+
+for row in reader:
+  x.append(row[0] + row[1])
+  openPrice.append(row[2])
+  maxPrice.append(row[3])
+  minPrice.append(row[4])
+  closePrice.append(row[5])
+  volumeSold.append(row[6])
+
+windowSize = 9 * 2
+
+ net = buildNetwork(windowSize, windowSize * 4, windowSize * 4, 1, hiddenclass=SoftmaxLayer)
+ds = SupervisedDataSet(windowSize, 1)
+
+n = windowSize * 8
+
+for i in range(n):
+    if i >= windowSize:
+      ds.addSample(([closePrice[i - (windowSize - j)] for j in range(windowSize)]), (closePrice[i]))
+
+# trainer = BackpropTrainer(net, ds) #, learningrate=0.01, lrdecay=1.0, momentum=0.0, weightdecay=0.0)
+trainer = RPropMinusTrainer(net, dataset=ds)
+for i in range(1000):
+    print('error: ', trainer.train(), '\n')
+
+print('error: ', trainer.trainUntilConvergence())
+
+y = []
+y_n = []
+
+for i in range(n):
+    y.append(closePrice[i])
+    if i < windowSize:
+      y_n.append(closePrice[i])
+    else:
+      r = net.activate([closePrice[i - (windowSize - j)] for j in range(windowSize)])
+      y_n.append(r[0])
+
+fig = plt.figure()
+# plt.plot(maxPrice, '')
+# plt.plot(minPrice, '')
+# plt.plot(openPrice, '')
+plt.plot(y, 'b')
+plt.plot(y_n, 'r')
+plt.show()
