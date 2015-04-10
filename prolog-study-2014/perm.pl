20b5ac512874ccc6d54b91c64882c5ca49233592
Denis Rotanov <mapholameth@gmail.com>
2015-04-10 14:42:31 +1000

    testcase for prolog permutations

--- prolog-study-2014/perm.pl
+++ prolog-study-2014/perm.pl
@@ -7,3 +7,11 @@ permute([X], [X]).
 permute([X|Y], Z) :-
   permute(Y, Z1),
   do_insert(X, Z1, Z).
+
+test_perm :-
+  findall(R, permute([1,2,3,4], R), Y),
+  length(Y, Len),
+  writef('%w solutions:\n', [Len]),
+  writef('  - %w\n', [Y]),
+  fail.
+test_perm.
